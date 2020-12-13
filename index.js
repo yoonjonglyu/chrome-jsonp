6edf397fa049b2aa9f07d368a23a0d0068f2c80e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-13 16:17:21 +0900

    feat: JSONP

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const head = document.querySelector("head");
+const body = document.querySelector("body");
 const button = document.querySelector('#jsonpStart');
+
+function jsonp () {
+    const script = document.createElement("script");
+    script.src = 'https://yoonjonglyu.github.io/webPage/assets/js/basic.js';
+    head.appendChild(script);
+    return 0;
+};
 
 button.addEventListener('click', () => {
     console.log("start");
+    jsonp();
 });
